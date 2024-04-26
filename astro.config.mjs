9ecd48568fe9bd2e413739b9e40411c33afbe5cc
Gary Díaz <garyking1982@gmail.com>
2024-04-25 23:13:34 -0400

    añadiendo ViewTransition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://garykingblog.netlify.app",
+  integrations: [preact()]
 });
